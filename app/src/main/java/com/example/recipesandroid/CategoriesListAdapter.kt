d32820f4bc2aadfb788485ace4e7088e3a799a91
JohnnyGomezzz <seldesign@mail.ru>
2025-01-29 22:59:24 +0300

    RA-08 adapter binding correction

--- app/src/main/java/com/example/recipesandroid/CategoriesListAdapter.kt
+++ app/src/main/java/com/example/recipesandroid/CategoriesListAdapter.kt
@@ -3,32 +3,25 @@ package com.example.recipesandroid
 import android.graphics.drawable.Drawable
 import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.recipesandroid.databinding.FragmentCategoriesListBinding
 import com.example.recipesandroid.databinding.ItemCategoryBinding
 
 class CategoriesListAdapter(private val dataSet: List<Category>) :
     RecyclerView.Adapter<CategoriesListAdapter.ViewHolder>() {
 
-    private var _binding: ItemCategoryBinding? = null
-    private val binding
-        get() = _binding ?: throw IllegalStateException("Binding for ItemCategoryBinding must not be null")
-
-
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val imageView: ImageView = view.findViewById(R.id.imCategory)
-        val titleTextView: TextView = view.findViewById(R.id.tvTitle)
-        val titleDescriptionView: TextView = view.findViewById(R.id.tvDescription)
+    class ViewHolder(binding: ItemCategoryBinding) : RecyclerView.ViewHolder(binding.root) {
+        val imageView: ImageView = binding.imCategory
+        val titleTextView: TextView = binding.tvTitle
+        val titleDescriptionView: TextView = binding.tvDescription
     }
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
         val inflater = LayoutInflater.from(viewGroup.context)
-        _binding = ItemCategoryBinding.inflate(inflater, viewGroup, false)
-        return ViewHolder(binding.root)
+        val binding = ItemCategoryBinding.inflate(inflater, viewGroup, false)
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
