a6973b9aea1d1119cde4bc9fd446d3ece89ca442
JohnnyGomezzz <seldesign@mail.ru>
2025-02-02 22:43:33 +0300

    RA-11 recipes list done

--- app/src/main/java/com/example/recipesandroid/RecipesListAdapter.kt
+++ app/src/main/java/com/example/recipesandroid/RecipesListAdapter.kt
@@ -8,10 +8,10 @@ import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.recipesandroid.databinding.ItemCategoryBinding
+import com.example.recipesandroid.databinding.ItemRecipeBinding
 
-class CategoriesListAdapter(private val dataSet: List<Category>) :
-    RecyclerView.Adapter<CategoriesListAdapter.ViewHolder>() {
+class RecipesListAdapter(private val dataSet: List<Recipe>) :
+    RecyclerView.Adapter<RecipesListAdapter.ViewHolder>() {
 
     private var itemClickListener: OnItemClickListener? = null
 
@@ -19,41 +19,39 @@ class CategoriesListAdapter(private val dataSet: List<Category>) :
         itemClickListener = listener
     }
 
-    class ViewHolder(binding: ItemCategoryBinding) : RecyclerView.ViewHolder(binding.root) {
-        val layoutCategory: LinearLayout = binding.layoutCategory
-        val imageView: ImageView = binding.imCategory
-        val titleTextView: TextView = binding.tvTitle
-        val titleDescriptionView: TextView = binding.tvDescription
+    class ViewHolder(binding: ItemRecipeBinding) : RecyclerView.ViewHolder(binding.root) {
+        val layoutRecipe: LinearLayout = binding.layoutRecipe
+        val imageView: ImageView = binding.imRecipe
+        val titleTextView: TextView = binding.tvTitleRecipe
     }
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
         val inflater = LayoutInflater.from(viewGroup.context)
-        val binding = ItemCategoryBinding.inflate(inflater, viewGroup, false)
+        val binding = ItemRecipeBinding.inflate(inflater, viewGroup, false)
         return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
-        val category: Category = dataSet[position]
+        val recipe: Recipe = dataSet[position]
 
-        viewHolder.titleTextView.text = category.title
-        viewHolder.titleDescriptionView.text = category.description
+        viewHolder.titleTextView.text = recipe.title
 
         val drawable =
             try {
-                Drawable.createFromStream(category.imageUrl?.let {
+                Drawable.createFromStream(recipe.imageUrl?.let {
                     viewHolder.itemView.context.assets.open(
                         it
                     )
                 }, null)
             } catch (e: Exception) {
-                Log.d("!!!", "Image not found: ${category.imageUrl}")
+                Log.d("!!!", "Image not found: ${recipe.imageUrl}")
                 null
             }
         viewHolder.imageView.setImageDrawable(drawable)
-        viewHolder.imageView.setContentDescription(category.title)
+        viewHolder.imageView.setContentDescription(recipe.title)
 
-        viewHolder.layoutCategory.setOnClickListener {
-            itemClickListener?.onItemClick(category.id)
+        viewHolder.layoutRecipe.setOnClickListener {
+            itemClickListener?.onItemClick(recipe.id)
         }
     }
 
--- app/src/main/java/com/example/recipesandroid/RecipesListFragment.kt
+++ app/src/main/java/com/example/recipesandroid/RecipesListFragment.kt
@@ -1,21 +1,29 @@
 package com.example.recipesandroid
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+import com.example.recipesandroid.Constants.ARG_CATEGORY_ID
+import com.example.recipesandroid.Constants.ARG_CATEGORY_IMAGE_URL
+import com.example.recipesandroid.Constants.ARG_CATEGORY_NAME
 import com.example.recipesandroid.databinding.FragmentRecipesListBinding
 
 class RecipesListFragment : Fragment() {
 
     private var _binding: FragmentRecipesListBinding? = null
     private val binding
-        get() = _binding ?: throw IllegalStateException("Binding for FragmentRecipesListBinding must not be null")
+        get() = _binding
+            ?: throw IllegalStateException("Binding for FragmentRecipesListBinding must not be null")
 
-    private var categoryId: String? = null
-    var categoryName: String? = null
-    var categoryImageUrl: String? = null
+    private var categoryId: Int? = null
+    private var categoryName: String? = null
+    private var categoryImageUrl: String? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,9 +36,47 @@ class RecipesListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        categoryId = requireArguments().getString(categoryId)
-        categoryName = requireArguments().getString(categoryName)
-        categoryImageUrl = requireArguments().getString(categoryImageUrl)
+        categoryId = requireArguments().getInt(ARG_CATEGORY_ID)
+        categoryName = requireArguments().getString(ARG_CATEGORY_NAME)
+        categoryImageUrl = requireArguments().getString(ARG_CATEGORY_IMAGE_URL)
+
+        initRecycler()
+
+        val drawable =
+            try {
+                Drawable.createFromStream(categoryImageUrl?.let {
+                    view.context.assets.open(
+                        it
+                    )
+                }, null)
+            } catch (e: Exception) {
+                Log.d("!!!", "Image not found: $categoryImageUrl")
+                null
+            }
+        binding.imageRecipes.setImageDrawable(drawable)
+        binding.tvRecipes.text = categoryName
+
+    }
+
+    private fun initRecycler() {
+        val recipesAdapter = RecipesListAdapter(STUB.getRecipesByCategoryId(0))
+        binding.rvRecipes.adapter = recipesAdapter
+
+        recipesAdapter.setOnItemClickListener(
+            object : OnItemClickListener {
+                override fun onItemClick(id: Int) {
+                    openRecipeByRecipeId(id)
+                }
+            }
+        )
+    }
+
+    private fun openRecipeByRecipeId(recipeId: Int) {
+        parentFragmentManager.commit {
+            replace<RecipeFragment>(R.id.mainContainer)
+            setReorderingAllowed(true)
+            addToBackStack(null)
+        }
     }
 
     override fun onDestroy() {
