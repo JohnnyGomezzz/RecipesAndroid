ccbd76419fe55013840f531fa24c16684edfd9f6
JohnnyGomezzz <seldesign@mail.ru>
2025-02-23 19:17:50 +0300

    RA-18 favorites screen

--- app/src/main/java/com/example/recipesandroid/FavoritesListFragment.kt
+++ app/src/main/java/com/example/recipesandroid/FavoritesListFragment.kt
@@ -1,17 +1,26 @@
 package com.example.recipesandroid
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+import com.example.recipesandroid.Constants.ARG_RECIPE
+import com.example.recipesandroid.Constants.FAVORITES_PREFS_FILE_KEY
+import com.example.recipesandroid.Constants.FAVORITE_RECIPES_KEY
 import com.example.recipesandroid.databinding.FragmentFavoritesListBinding
 
 class FavoritesListFragment : Fragment() {
 
     private var _binding: FragmentFavoritesListBinding? = null
     private val binding
-        get() = _binding ?: throw IllegalStateException("Binding for FragmentFavoritesListBinding must not be null")
+        get() = _binding
+            ?: throw IllegalStateException("Binding for FragmentFavoritesListBinding must not be null")
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,6 +29,55 @@ class FavoritesListFragment : Fragment() {
     ): View {
         _binding = FragmentFavoritesListBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initRecycler()
+    }
+
+    private fun getFavorites(): HashSet<Int> {
+        val sharedPref: SharedPreferences by lazy {
+            context?.getSharedPreferences(FAVORITES_PREFS_FILE_KEY, Context.MODE_PRIVATE)
+                ?: throw IllegalStateException(
+                    "Fragment $this not attached to a context."
+                )
+        }
+        val setOfString = sharedPref.getStringSet(FAVORITE_RECIPES_KEY, null)?.let { HashSet(it) }
+            ?: hashSetOf()
+        val setOfInt = hashSetOf<Int>()
+        setOfString.forEach { setOfInt.add(it.toInt()) }
+        return setOfInt
+    }
+
+    private fun initRecycler() {
+        val idsSet = STUB.getRecipesByIds(getFavorites())
+        val recipesAdapter = RecipesListAdapter(idsSet)
+        binding.rvFavoritesList.adapter = recipesAdapter
+
+        recipesAdapter.setOnItemClickListener(
+            object : OnItemClickListener {
+                override fun onItemClick(id: Int) {
+                    openRecipeByRecipeId(id)
+                }
+            }
+        )
+        if (getFavorites().isNotEmpty()) {
+            binding.rvFavoritesList.visibility = View.VISIBLE
+            binding.tvNoFavorites.visibility = View.GONE
+        }
+    }
+
+    private fun openRecipeByRecipeId(recipeId: Int) {
+        val recipe = STUB.getRecipeById(recipeId)
+        val bundle = bundleOf(ARG_RECIPE to recipe)
+
+        parentFragmentManager.commit {
+            replace<RecipeFragment>(R.id.mainContainer, args = bundle)
+            setReorderingAllowed(true)
+            addToBackStack(null)
+        }
     }
 
     override fun onDestroy() {
